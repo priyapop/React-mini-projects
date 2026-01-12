0b4d12595e88ebf5cd307973909d96f06939c532
priyapop <dangolpriya21@gmail.com>
2026-01-12 20:49:45 +0545

    step progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import PaginationTest from "./components/pagination/test";
 import Clock from "./components/digatal-clock";
 import CountdownTimerTest from "./components/countdown-timer/test";
+import StepProgressBarTest from "./components/step-progress-bar/test";
 function App() {
   return (
     <>
       <div className="App">
         {/* <PaginationTest /> */}
       {/* <Clock/> */}
-        <CountdownTimerTest/>
+        {/* <CountdownTimerTest/> */}
+        <StepProgressBarTest/>
       </div>
     </>
   );
---
+++ src/components/step-progress-bar/test.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import StepProgressBar from './index.jsx'
+import './progress.css'
+function StepProgressBarTest() {
+  const [activeStep, setActiveStep] = useState(0);
+  const steps = ["Step 1", "Step 2", "step 3", "Step 4", "Step 5"];
+  return (
+    <div className="step-progress-bar-container">
+        <h1>Step Progress Bar</h1>
+        <StepProgressBar setActiveStep={setActiveStep} steps={steps} activeStep={activeStep}/>
+
+    </div>
+  );
+}
+
+export default StepProgressBarTest;
